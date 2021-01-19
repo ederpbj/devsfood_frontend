abd6bb1390aa063383b8d5e2df04c2f334debbee
ederpbj <ederpbj@gmail.com>
2021-01-19 11:31:11 -0300

    Base para projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+## Projeto delivery
+
+## Links
+
+[Explicação do Projeto](https://alunos.b7web.com.br/curso/reactjs/devsfood-explicacao-do-projeto)
+
+
+[Git hub](https://github.com/ederpbj/devsfood_frontend/tree/main/src)
+
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+
+import HomeScreen from "./pages/HomeScreen";
+import Tela2Screen from "./pages/Tela2Screen";
+
+import { Container, Menu, PageBody } from './AppStyled';
+
+export default () => {
+  const name = useSelector((state) => state.user.name);
+  //test
+  return (
+    <BrowserRouter>
+      <h1>Oi, {name} </h1>
+      <Container>
+        <Menu>
+
+        </Menu>
+        <PageBody>
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+            <Route path="/tela2/:nome">
+              <Tela2Screen />
+            </Route>
+          </Switch>
+        </PageBody>
+
+      </Container>
+
+      <h1>Oi, {name}</h1>
+
+    </BrowserRouter>
+  );
+};
